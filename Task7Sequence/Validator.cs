c6d88bbc3d5c6a8aeeba3f6bf6a90aa03f8e0f43
PavelTcsiposchuk <48702275+PavelTcsiposchuk@users.noreply.github.com>
2019-06-01 17:57:43 +0300

    /////

---
+++ Task7Sequence/Validator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7Sequence
+{
+    public class Validator
+    {
+        public static bool IsValidNumber(string number, out int result)
+        {
+            return int.TryParse(number, out result);
+        }
+    }
+}
