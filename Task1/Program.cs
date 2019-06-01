19fd9b32998d8da84d13a78213bda091473d0293
PavelTcsiposchuk <48702275+PavelTcsiposchuk@users.noreply.github.com>
2019-06-01 17:49:48 +0300

    основной функционал есть, нужно ревью

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,49 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] readyArgs = GetReadyArgs(args);
+            Board board = new Board(readyArgs[0], readyArgs[1]);
+            UI.PrintBoard(board);
+
+            Console.ReadLine();
+        }
+
+        private static int[] GetReadyArgs(string[] args)
+        {
+            int[] readyArgs;
+            if (args.Length != 2)
+            {
+                UI.ShowInstruction();
+                readyArgs = GetArgsFromUI();
+            }
+            else
+            {
+                bool isFirstArgValid = Validator.IsValidNumber(args[0], out int firstArgument);
+                bool isSecondArgValid = Validator.IsValidNumber(args[1], out int secondArgument);
+                if (isFirstArgValid && isSecondArgValid)
+                {
+                    readyArgs = new int[] { firstArgument, secondArgument };
+                }
+                else
+                {
+                    UI.ShowError("Было указано неподходящее значение, попробуйте еще раз \n");
+                    readyArgs = GetArgsFromUI();
+                }
+            }
+
+            return readyArgs;
+        }
+
+        private static int[] GetArgsFromUI()
+        {
+            int[] readyArgs = new int[2];
+
+            UI.ShowMessage("Нужно задать ширину.");
+            readyArgs[0] = UI.ReadParametr();
+            UI.ShowMessage("Нужно задать высоту.");
+            readyArgs[1] = UI.ReadParametr();
+            
+            return readyArgs;
         }
     }
 }
---
+++ Task1/Validator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class Validator
+    {
+        public static bool IsValidNumber(string number, out int result)
+        {
+            return Int32.TryParse(number, out result) && result >= 1 && result <= 32;
+        }
+    }
+}
