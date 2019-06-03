37b0f1dbb30c0119b3c943410e57888b33429e84
PavelTcsiposchuk <48702275+PavelTcsiposchuk@users.noreply.github.com>
2019-06-04 02:43:22 +0300

    начинаю

---
+++ Task8FibonacciSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task8FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = GetReadyArgument(args);
+            FibonacciSequence naturalNumberSequence = new FibonacciSequence(range);
+            UI.PrintResult(range, naturalNumberSequence.GetFibonacciSequence());
+
+            Console.ReadLine();
+        }
+
+        private static int GetReadyArgument(string[] args)
+        {
+            int readyArgs;
+
+            if (!Validator.CheckArgument(args,out readyArgs))
+            {
+                UI.ShowInstruction();
+                readyArgs = UI.GetNaturalNumberFromConsole();
+            }
+
+            return readyArgs;
+        }
+
+    }
+}
